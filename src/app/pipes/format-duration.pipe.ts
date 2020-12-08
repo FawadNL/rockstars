c6b55c3195e918f78e939008407c98be781b04cf
Fawad Tiemoerie <info@roadmapstudios.com>
2020-12-08 15:16:10 +0100

    pipe optimized

--- src/app/pipes/format-duration.pipe.ts
+++ src/app/pipes/format-duration.pipe.ts
@@ -11,12 +11,12 @@ export class FormatDurationPipe implements PipeTransform {
    * @param time This is songs duration in milliseconds.
    * @param args Additional arguments if needed.
    */
-  transform(time: number, ...args: any[]): unknown {
-    const Millisecond = 1000;
-    const Second = 60;
-    const MinuteDurationOfSong = Math.floor(time / Millisecond / Second);
-    const SecondDurationOfSong = Math.floor((time / Millisecond) % Second);
-    return ` ${MinuteDurationOfSong} : ${SecondDurationOfSong}`;
+  transform(time: number): string {
+    const millisecond = 1000;
+    const second = 60;
+    const minuteDurationOfSong = Math.floor(time / millisecond / second);
+    const secondDurationOfSong = Math.floor((time / millisecond) % second);
+    return ` ${minuteDurationOfSong} : ${secondDurationOfSong}`;
   }
 
 }
